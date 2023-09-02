cf5de12aaaa2285e87bf89a61e3a4b9fbb77c341
Michele Gambella <michele.gambella@gmail.com>
2023-09-02 15:55:55 +0200

    config upgrade

--- config/app.php
+++ config/app.php
@@ -15,10 +15,13 @@
  * If you want to modify the application config for *only* web requests or
  * *only* console requests, create an app.web.php or app.console.php file in
  * your config/ folder, alongside this one.
+ *
+ * Read more about application configuration:
+ * https://craftcms.com/docs/4.x/config/app.html
  */
 
 use craft\helpers\App;
 
 return [
-    'id' => App::env('APP_ID') ?: 'CraftCMS',
+    'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
 ];
--- config/general.php
+++ config/general.php
@@ -11,15 +11,24 @@
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-$isDev = App::env('ENVIRONMENT') === 'dev';
-$isProd = App::env('ENVIRONMENT') === 'production';
-
 return GeneralConfig::create()
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
-    ->cpTrigger(App::env('CP_TRIGGER') ?: 'admin')
-    ->securityKey(App::env('SECURITY_KEY'))
-    ->devMode($isDev)
-    ->allowAdminChanges($isDev)
-    ->disallowRobots(!$isProd)
+    // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+    ->devMode(App::env('DEV_MODE') ?? false)
+    // Preload Single entries as Twig variables
+    ->preloadSingles()
+    // Allow administrative changes
+    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
+    // Disallow robots
+    ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    // Prevent user enumeration attacks
+    ->preventUserEnumeration()
+    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->aliases([
+        '@web' => App::env('CRAFT_WEB_URL'),
+        '@webroot' => dirname(__DIR__) . '/web',
+    ])
     ->headlessMode(true);
